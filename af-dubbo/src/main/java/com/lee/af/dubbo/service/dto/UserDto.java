8e40625d0829f204a502ea819893194ef4425f9c
wandou1998 <1085048619@qq.com>
2026-03-02 12:13:39 +0800

    -dubbo对象序列化 -update pom.xml

--- af-dubbo/src/main/java/com/lee/af/dubbo/service/dto/UserDto.java
+++ af-dubbo/src/main/java/com/lee/af/dubbo/service/dto/UserDto.java
@@ -12,6 +12,11 @@ import java.io.Serializable;
 @Data
 public class UserDto implements Serializable {
     // 显式声明序列化版本号（建议手动指定，避免自动生成导致不一致）
+    /**
+     * 序列化过程中会将对象的状态（包括类名和serialVersionUID）写入字节流，反序列化时，JVM会加载当前的类，并将类中声明的serialVersionUID
+     * 与字节流中存储的版本号进行比较。如果两者完全匹配，反序列化才能成功进行。
+     * serialVersionUID 本质上是序列化类的“版本号”，它通过显式控制版本标识，确保了在类定义发生变化时，反序列化操作仍能安全、可靠地进行
+     */
     @Serial
     private static final long serialVersionUID = 1L;
 
